04c5f6f1ed4151cdc37ee8c9f1d53a6a325281b7
Bailey Bryant <bailey.bryant10@gmail.com>
2017-10-08 22:53:58 -0500

    portfolio page details

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,9 +11,19 @@ class Portfolio extends Component {
       <div>
       <h1>  My Projects </h1>
       <div className="portfolio">
+      <h3> Online Furniture </h3>
       <img src={Ecom} height= "400px" width="400px" className="portfolio-image"/>
+      <p> this project...</p>
+      <p> Technology used: HTML5, JavaScript, React, Redux, CSS</p>
+      <h3> Code Snippets</h3>
       <img src={MusicReview} height= "400px" width="400px" className="portfolio-image"/>
+      <p>this project...</p>
+      <p> Technology used: JavaScript, MongoDB, Mongoose, Express.js, Mustache </p>
+      <h3> Music Share </h3>
       <img src={Ecom} height= "400px" width="400px" className="portfolio-image"/>
+      <p>  this project...</p>
+      <p> Technology used: HTML5, JavaScript, React, Bootstrap, CSS  </p>
+
       </div>
 
 
