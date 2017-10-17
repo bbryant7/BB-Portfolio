356d2029c5cefb7a70b8f21b927283eb8e87b80d
Bailey Bryant <bailey.bryant10@gmail.com>
2017-10-17 09:30:36 -0500

    change underline on nave bar, add spacing between sections update project layouts

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,9 +12,9 @@ class Portfolio extends Component {
 
         <div className="project">
           <h3> Online Furniture </h3>
+          <p className="project-info"> An online shopping resource for home goods.</p>
           <img src={Ecom} className="portfolio-image"/>
-          <p> An online shopping resource for home goods.</p>
-          <p> <span className="bold"> Technology used:</span> HTML5, JavaScript, React, React-Router-Dom, CSS</p>
+          <p className="technology"> <span className="bold"> Technology used:</span> HTML5, JavaScript, React, React-Router-Dom, CSS</p>
           <div className="project-links">
             <a href="x"> Live Link </a>
             <a href="https://github.com/bbryant7/e-com-site"> Code</a>
@@ -23,18 +23,18 @@ class Portfolio extends Component {
         </div>
         <div className="project">
           <h3> Code Snippets</h3>
+          <p className="project-info">An app to help developers organize snippets of code. </p>
           <img src={MusicReview}  className="portfolio-image"/>
-          <p><span className="bold"> The Project: </span>An app to help developers organize snippets of code. </p>
-          <p> <span className="bold"> Technology used:</span> JavaScript, MongoDB, Mongoose, Express.js, Mustache </p>
+          <p className="technology"> <span className="bold"> Technology used:</span> JavaScript, MongoDB, Mongoose, Express.js, Mustache </p>
           <div className="project-links">
             <a href="https://github.com/bbryant7/Code-Snippet-App"> Code</a>
           </div>
         </div>
         <div className="project">
           <h3> Music Share </h3>
+          <p className="project-info"> An app to share music recommendations.</p>
           <img src={MusicReview} className="portfolio-image"/>
-          <p> <span className="bold"> The Project: </span> An app to share music recommendations.</p>
-          <p> <span className="bold"> Technology used:</span> HTML5, JavaScript, React, Bootstrap, CSS  </p>
+          <p className="technology"> <span className="bold"> Technology used:</span> HTML5, JavaScript, React, Bootstrap, CSS  </p>
           <div className="project-links">
             <a href="https://bbryant7.github.io/Music-Review/"> Live Link </a>
             <a href="https://github.com/bbryant7/Music-Review"> Code</a>
