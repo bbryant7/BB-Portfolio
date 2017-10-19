2139cbf9308b2438af5d72115975cb8e3fb9c9a7
Bailey Bryant <bailey.bryant10@gmail.com>
2017-10-19 10:27:53 -0500

    add content to about

--- src/components/About.js
+++ src/components/About.js
@@ -15,9 +15,12 @@ class About extends Component {
 
           <h2> I’m Bailey, and I’m really glad to meet you. </h2>
 
-          <p> Born and raised in Austin, Texas, I’m an original Austinite. My professional career has been a rewarding, winding road embarking with a history degree and making pit-stops in sales, recruiting and event planning before landing  me here, as a Web Developer. </p>
+          <p>Born and raised in Austin, Texas, I’m an original Austinite. My professional career has been a rewarding, winding road embarking from UT with a history degree and making pit stops in sales, recruiting and event coordinating. At each stop I gathered new skills and learned more about myself as a professional, but I was not able to create at work.</p>
+          <p>While working as an event coordinator, I had the opportunity to work with talented developers and designers. During this time I realized that this was the creative outlet I’d been looking for. The question was “how do I make this happen for myself?” </p>
+          <p> I spent 16 weeks at The Iron Yard fully immersed in web development, spending 70+ hours a week honing my skills. I worked with a full-stack spectrum of technologies including HTML/CSS, JavaScript, Node.js, MongoDB, PostgreSQL, and Express.js after which, I chose to focus on front end, learning React and Redux.</p>
+          <p>Through code I am able to combine my natural curiosity and logical nature with my desire to create. I find myself surrounded by like-minded individuals that love to learn as part of their craft.</p>
+          <p>When I’m not coding, you’ll find me relaxing at the green belt, playing board games, fostering sweet kittens, making costumes and traveling. My latest adventure took me to Taipei, Taiwan and Hong Kong.</p>
 
-          <p> When I’m not coding, you’ll find me relaxing at local breweries, playing board-games, fostering sweet kittens, making costumes and traveling. My latest adventure took me to me to Taipei, Taiwan and Hong Kong.</p>
         </div>
       </div>
     );
